5168dbadfcb71f05c411a9bc99510e5bc97a3c4c
Shanmukha Srinivas Ch <shanmukhasrinivas82@gmail.com>
2025-06-16 00:16:27 +0530

    started adding new templates

--- menu/admin.py
+++ menu/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.utils.html import format_html
 from django.urls import reverse
-from .models import MenuLink, MenuItem, MenuSection, Restaurant
+from .models import MenuLink, MenuItem, MenuSection, Restaurant, MenuTemplate
 
 class MenuItemInline(admin.TabularInline):
     model = MenuItem
@@ -30,4 +30,20 @@ class MenuSectionAdmin(admin.ModelAdmin):
 class MenuItemAdmin(admin.ModelAdmin):
     list_display = ('name', 'menu', 'section', 'price', 'is_available')
     list_filter = ('menu', 'section', 'is_available')
-    search_fields = ('name', 'description') 
+    search_fields = ('name', 'description')
+
+@admin.register(MenuTemplate)
+class MenuTemplateAdmin(admin.ModelAdmin):
+    list_display = ('name', 'template_file', 'is_active', 'created_at', 'updated_at')
+    list_filter = ('is_active',)
+    search_fields = ('name', 'description', 'template_file')
+    readonly_fields = ('created_at', 'updated_at')
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'template_file', 'description', 'thumbnail', 'is_active')
+        }),
+        ('Timestamps', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+    ) 
